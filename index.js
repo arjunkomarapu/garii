40f141421500510881db986d48506ac6e9e51cf8
arjunkomarapu <42857105+arjunkomarapu@users.noreply.github.com>
2019-11-13 16:27:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,65 @@ restService.use(
     extended: true
   })
 );
+//creating database connection
+const conn = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'Needyin@123',
+  database: 'needyin'
+});
+//connecting to database
+conn.connect((err) =>{
+  if(err) throw err;
+  console.log('Mysql Connected...');
+});
+
+restService.get('/api/products/:id',(req, res) => {
+  let sql = "SELECT * FROM product WHERE product_id="+req.params.id;
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  });
+});
+
+//show all products
+restService.get('/api/products',(req, res) => {
+  let sql = "SELECT * FROM product";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  });
+});
+
+restService.get('/api/products', (req, res) => {
+
+    const productSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.products ? req.body.result.parameters.products : 'The Godfather';
+    const reqUrl = encodeURI(`https://1b702621.ngrok.io/api/products`);
+    http.get(reqUrl, (responseFromAPI) => {
+        let completeResponse = '';
+        responseFromAPI.on('data', (chunk) => {
+            completeResponse += chunk;
+        });
+        responseFromAPI.on('end', () => {
+            const movie = JSON.parse(completeResponse);
+            let dataToSend = productSearch === 'show me my products' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
+            dataToSend += `${products.product_name} is a ${product.product_price}`;
+
+            return res.json({
+                speech: dataToSend,
+                displayText: dataToSend,
+                source: 'webhook-echo-sample'
+            });
+        });
+    }, (error) => {
+        return res.json({
+            speech: 'Something went wrong!',
+            displayText: 'Something went wrong!',
+            source: 'webhook-echo-sample'
+        });
+    });
+});
+
 
 restService.use(bodyParser.json());
 
@@ -47,28 +106,7 @@ restService.post("/echo", function(req, res) {
   });
 });
 
-//creating database connection
-const conn = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'Needyin@123',
-  database: 'needyin'
-});
-//connecting to database
-conn.connect((err) =>{
-  if(err) throw err;
-  console.log('Mysql Connected...');
-});
-//show all products
-restService.get('/api/products',(req, res) => {
-  let sql = "SELECT * FROM product";
-  let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
-    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-  });
-});
 
-
-restService.listen(process.env.PORT || 8000, function() {
+restService.listen(process.env.PORT || 6000, function() {
   console.log("Server up and listening");
 });
